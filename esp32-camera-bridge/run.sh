1debd4255253b280054be5d38f1cbf27135361ed
Richard <42013503+21Chard@users.noreply.github.com>
2026-06-28 22:06:44 +0800

    Update run.sh

--- esp32-camera-bridge/run.sh
+++ esp32-camera-bridge/run.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/with-contenv bash
 
-echo "########################################"
-echo "ESP32 CAMERA BRIDGE STARTED"
-echo "########################################"
-
-ls -l /
-
-echo "Python:"
-which python3
-python3 --version
-
-echo "Pip packages:"
-python3 -m pip list
-
-sleep 300
+exec python3 /server.py
